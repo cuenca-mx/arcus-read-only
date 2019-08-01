06cc9604943c7703f34ea6d20ce099453069ddca
Jose Gregorio Castro Lazo <josegregorio994@gmail.com>
2019-08-01 17:47:32 -0500

    remove unnecessary lines

--- arcus_read_only/app.py
+++ arcus_read_only/app.py
@@ -12,16 +12,10 @@ def make_response(status_code: int, body: dict) -> dict:
 
 
 def lambda_handler(event, context):
-    if event['httpMethod'] == 'GET':
-        try:
-            print(arcus_api_key)
-            print(arcus_secret_key)
-            sandbox = event['headers']['X-Arcus-Sandbox'] == 'true'
-            client = Client(arcus_api_key, arcus_secret_key, sandbox=sandbox)
-            response = client.get(event['path'])
-            return make_response(200, response)
-        except Exception as ex:
-            print(f'Error: {ex}')
-            return make_response(400, dict(message='Bad Request'))
-    else:
-        return make_response(405, dict(message='Method Not Allowed'))
+    try:
+        sandbox = event['headers']['X-Arcus-Sandbox'] == 'true'
+        client = Client(arcus_api_key, arcus_secret_key, sandbox=sandbox)
+        response = client.get(event['path'])
+        return make_response(200, response)
+    except Exception as ex:
+        return make_response(400, dict(message='Bad Request'))
