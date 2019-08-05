9ae15a16ab31e5177a4e483fc8c081f780988847
Jose Gregorio Castro Lazo <josegregorio994@gmail.com>
2019-08-05 17:41:18 -0500

    use query params

--- arcus_read_only/app.py
+++ arcus_read_only/app.py
@@ -15,7 +15,10 @@ def lambda_handler(event, context):
     try:
         sandbox = event['headers']['X-ARCUS-SANDBOX'] == 'true'
         client = Client(arcus_api_key, arcus_secret_key, sandbox=sandbox)
-        response = client.get(event['path'])
+        path = event['path']
+        if event['queryStringParameters'] and 'page' in event['queryStringParameters']:
+            path += f'?page={event["queryStringParameters"]["page"]}'
+        response = client.get(path)
         return make_response(200, response)
     except Exception as ex:
         return make_response(400, dict(message='Bad Request'))
